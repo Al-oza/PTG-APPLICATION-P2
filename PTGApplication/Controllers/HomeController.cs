b1eb0967451588c919865697f98eab53f422c82c
Nicholas Gorden <ngorden1997@gmail.com>
2019-09-17 21:42:43 -0500

    Add Basic About Page

--- PTGApplication/Controllers/HomeController.cs
+++ PTGApplication/Controllers/HomeController.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PTGApplication.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        public ActionResult About()
+        { return View(); }
         public ActionResult Index()
-        {
-            return View();
-        }
+        { return View(); }
     }
 }
