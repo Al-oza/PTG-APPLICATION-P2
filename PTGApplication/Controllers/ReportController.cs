b4309714991da9793583167c1a01c7a71169d292
cstrathman <carli-strathman@uiowa.edu>
2019-09-30 13:57:55 -0500

    Change in Views and Links

---
+++ PTGApplication/Controllers/ReportController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace PTGApplication.Controllers
+{
+    [Authorize]
+    public class ReportController : Controller
+    {
+        public ActionResult About()
+        { return View(); }
+      
+        public ActionResult Index()
+        { return View(); }
+    }
+}
