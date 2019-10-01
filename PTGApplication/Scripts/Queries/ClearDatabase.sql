315280c9ee0f8b65a5c478069680cfaf0b952890
Nicholas Gorden <ngorden1997@gmail.com>
2019-10-01 13:47:03 -0500

    Clear Database

---
+++ PTGApplication/Scripts/Queries/ClearDatabase.sql
@@ -0,0 +1,9 @@
+USE PilaneDB;
+GO
+
+DELETE FROM [dbo].[__MigrationHistory];
+DELETE FROM [dbo].[AspNetRoles];
+DELETE FROM [dbo].[AspNetUserClaims];
+DELETE FROM [dbo].[AspNetUserLogins];
+DELETE FROM [dbo].[AspNetUserRoles];
+DELETE FROM [dbo].[AspNetUsers];
