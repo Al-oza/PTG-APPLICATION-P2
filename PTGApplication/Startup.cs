ba77c074c57b4d1012233dff0479bcc0923a2d31
Nicholas Gorden <ngorden1997@gmail.com>
2019-10-01 12:34:18 -0500

    Start User Roles

--- PTGApplication/Startup.cs
+++ PTGApplication/Startup.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Web.Configuration;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Owin;
+using PTGApplication.Models;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Configuration;
 
 [assembly: OwinStartup(typeof(PTGApplication.Startup))]
 
@@ -17,7 +18,35 @@ namespace PTGApplication
             var configuration = WebConfigurationManager.OpenWebConfiguration("~");
             var section = (ConnectionStringsSection)configuration.GetSection("connectionStrings");
             section.ConnectionStrings["DefaultConnection"].ConnectionString = Properties.SharedResources.DbConnection;
+
             ConfigureAuth(app);
+            CreateRolesAndUsers().Wait();
+        }
+        private async Task CreateRolesAndUsers()
+        {
+            var context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                roleManager.Create(new IdentityRole() { Name = "Admin" });
+
+                var user = new ApplicationUser()
+                {
+                    EmailConfirmed = true,
+                    Email = Properties.SharedResources.DefaultEmail,
+                    UserName = Properties.SharedResources.DefaultUser
+                };
+                var chkUser = await userManager.CreateAsync(user, Properties.SharedResources.DefaultPass);
+                if (chkUser.Succeeded) { await userManager.AddToRoleAsync(user.Id, "Admin"); }
+            }
+
+            if(!await roleManager.RoleExistsAsync("Manager"))
+            { await roleManager.CreateAsync(new IdentityRole() { Name = "Manager" }); }
+
+            if(!await roleManager.RoleExistsAsync("Employee"))
+            { await roleManager.CreateAsync(new IdentityRole() { Name = "Employee" }); }
         }
     }
 }
