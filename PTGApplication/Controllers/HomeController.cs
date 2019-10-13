52b9f44633c1079605b6fb5bd15c9963dfbfa829
cstrathman <carli-strathman@uiowa.edu>
2019-10-13 16:27:31 -0500

    Changed Homepage to only one page. Added support email link.

--- PTGApplication/Controllers/HomeController.cs
+++ PTGApplication/Controllers/HomeController.cs
@@ -5,8 +5,7 @@ namespace PTGApplication.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        public ActionResult About()
-        { return View(); }
+      
         public ActionResult Index()
         { return View(); }
     }
