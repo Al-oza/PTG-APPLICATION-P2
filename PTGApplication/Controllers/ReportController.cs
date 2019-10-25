1c92a9217e76c379c12c5624a39553f78ef30062
cstrathman <carli-strathman@uiowa.edu>
2019-10-25 16:02:12 -0500

    PENDING ORDERS DONE!!

--- PTGApplication/Controllers/ReportController.cs
+++ PTGApplication/Controllers/ReportController.cs
@@ -1,4 +1,5 @@
 ﻿using PTGApplication.Models;
+using PTGApplication.Providers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +17,15 @@ namespace PTGApplication.Controllers
 
         public ActionResult PendingOrders()
         {
-            return View();  
+            var query = "select UzimaDrug.DrugName AS 'Drug Name', COUNT(*) AS 'Quantity', CAST(FLOOR(CAST(DateOrdered AS float)) AS datetime) AS 'Ordered On', Username AS 'Ordered By', UzimaLocation.LocationName as 'Going To', ExpirationDate AS 'Expiring On' from[dbo].[UzimaInventory] join UzimaDrug on UzimaDrug.Id = UzimaInventory.DrugId LEFT JOIN AspNetUsers ON AspNetUsers.Id = UzimaInventory.LastModifiedBy LEFT JOIN UzimaLocation ON UzimaLocation.Id = UzimaInventory.FutureLocationId where FutureLocationId  is not null  And StatusId = 1 GROUP BY UzimaDrug.DrugName, CAST(FLOOR(CAST(DateOrdered AS float)) AS datetime), AspNetUsers.Username, UzimaLocation.LocationName, ExpirationDate, FutureLocationId ORDER BY UzimaInventory.FutureLocationId; ";
+            using (var dataSet = ConnectionPool.Query(query, "UzimaDrug", "UzimaInventory", "UzimaLocation", "AspNetUsers"))
+            {
+                ViewBag.Columns = dataSet.Tables[0].Columns;
+                ViewBag.Data = dataSet.Tables[0].Rows;
+                
+                return View();
+            }
         }
-
         public ActionResult ExpiredDrugs()
         {
             return View();
---
+++ PTGApplication/Providers/ConnectionPool.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace PTGApplication.Providers
+{
+    public class ConnectionPool : IDisposable
+    {
+        private readonly String _connectionString;
+        private ConnectionPool()
+        {
+            var configuration = WebConfigurationManager.OpenWebConfiguration("~");
+            var section = (ConnectionStringsSection)configuration.GetSection("connectionStrings");
+
+            _connectionString = section.ConnectionStrings["DefaultConnection"].ToString();
+        }
+        private void ConnectToDatabase()
+        {
+            DatabaseConnection = new SqlConnection(_connectionString);
+            DatabaseConnection.Open();
+        }
+        public static ConnectionPool GetConnectionPool()
+        {
+            var result = new ConnectionPool();
+            result.ConnectToDatabase();
+            return result;
+        }
+        public void Dispose()
+        {
+            DatabaseConnection.Close();
+            DatabaseConnection.Dispose();
+        }
+        public static DataSet Query(String sqlcommand, params string[] tables)
+        {
+            using (var connection = GetConnectionPool())
+            using (var adapter = new SqlDataAdapter())
+            using (var command = new SqlCommand(sqlcommand,
+                connection.DatabaseConnection))
+            {
+                command.CommandType = System.Data.CommandType.Text;
+                foreach (var table in tables)
+                {
+                    adapter.TableMappings.Add(table, table);
+                }
+                adapter.SelectCommand = command;
+
+                var dataSet = new DataSet("Report");
+                adapter.Fill(dataSet);
+
+                return dataSet;
+            }
+        }
+
+        public String ConnectionString { get { return _connectionString; } }
+        public SqlConnection DatabaseConnection { get; private set; }
+
+
+
+
+    }
+}
