100bdfdfa1df8702c674bf6de3e971bf0a0c40d7
Nicholas Gorden <ngorden1997@gmail.com>
2020-01-19 10:40:23 -0800

    Home Controller Commented

--- PTGApplication/Controllers/HomeController.cs
+++ PTGApplication/Controllers/HomeController.cs
@@ -3,8 +3,15 @@
 namespace PTGApplication.Controllers
 {
     [Authorize]
+    /// <summary>
+    /// Controller for Main/Home Page
+    /// </summary>
     public class HomeController : Controller
     { 
+        /// <summary>
+        /// Gets Home Page
+        /// </summary>
+        /// <returns>Home Page</returns>
         public ActionResult Index()
         { return View(); }
     }
