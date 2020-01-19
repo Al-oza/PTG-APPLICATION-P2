109d1f58aa83f08a7e650d8053e759c74dade1da
Nicholas Gorden <ngorden1997@gmail.com>
2020-01-19 11:35:38 -0800

    Me Controller Commented

--- PTGApplication/Controllers/MeController.cs
+++ PTGApplication/Controllers/MeController.cs
@@ -7,6 +7,10 @@ using System.Web.Http;
 namespace PTGApplication.Controllers
 {
     [Authorize]
+    /// <summary>
+    /// This class is generated. It is used to manage user sign ins and handles login cookies.
+    /// </summary>
+    /// <remarks cref="https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions-1/controllers-and-routing/aspnet-mvc-controllers-overview-cs" />
     public class MeController : ApiController
     {
         private ApplicationUserManager _userManager;
